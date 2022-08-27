fb64aa6539779e3b04dde134a04a1b62adb5a431
cslee <lcs5382@naver.com>
2022-08-27 15:50:26 +0900

    d

--- ch02/.ipynb_checkpoints/2장-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
-users = pd.read_csv('./data/u.user', sep='|', names=u_cols, encoding='latin-1')
-users = users.set_index('user_id')
-print(users.head())
